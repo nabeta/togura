76154b0e18c96b2dad2281648e2c78e8c758c0cb
Kosuke Tanabe <nabeta@fastmail.fm>
2025-09-13 20:39:53 +0900

    テストを修正

--- test/test_jpcoar.py
+++ test/test_jpcoar.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-import jpcoar
+import togura.jpcoar as jpcoar
 import pytest
 
 def test_resource_type_uri():
