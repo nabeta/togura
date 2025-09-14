00149d4e38279fc8d4070443d797032b4f2e66f9
Kosuke Tanabe <nabeta@fastmail.fm>
2025-09-14 23:48:56 +0900

    CIにリリース処理を追加

---
+++ tests/smoke_test.py
@@ -0,0 +1,7 @@
+import togura.jpcoar as jpcoar
+
+resource_type_uri = jpcoar.resource_type_uri("article")
+if resource_type_uri == "http://purl.org/coar/resource_type/c_6501":
+  print("Smoke test succeeded")
+else:
+  raise RuntimeError(resource_type_uri)
